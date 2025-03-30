53bd8aa6b88fe4ebae61b52c180923a75e189118
SamuelePassiatore <samuw.978@gmail.com>
2025-03-30 19:21:10 +0200

    update enable cors in main.ts

--- week-4/dapp-voting/packages/nestjs/src/main.ts
+++ week-4/dapp-voting/packages/nestjs/src/main.ts
@@ -7,11 +7,7 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
