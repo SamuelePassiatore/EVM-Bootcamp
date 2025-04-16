12510509fb0f686bd8569d2effcb01d8835e22aa
SamuelePassiatore <samuw.978@gmail.com>
2025-04-16 21:34:50 +0200

    update app

--- final-project/frontend/src/App.tsx
+++ final-project/frontend/src/App.tsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+        <div className="logo">
+          <img src="https://www.cryptologos.cc/logos/bitcoin-btc-logo.png?v=040" alt="Logo" />
+        </div>
+        <div className="app-name">
+          <h1>MyDApp</h1>
+        </div>
+        <div className="wallet-connect">
+          <button className="connect-button">Connect Wallet</button>
+        </div>
+      </header>
     </>
   )
 }
