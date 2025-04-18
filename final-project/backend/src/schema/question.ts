02ba0d649675464ff1e2b5673859a823d89f2e17
SamuelePassiatore <samuw.978@gmail.com>
2025-04-18 22:04:17 +0200

    add question model

---
+++ final-project/backend/src/schema/question.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IQuestion {
+  level: number;
+  text: string;
+  options: string[];
+  correctOptionIndex: number;
+  createdAt: Date;
+}
+
+const questionSchema = new Schema<IQuestion>({
+  level: {
+    type: Number,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  options: {
+    type: [String],
+    required: true,
+  },
+  correctOptionIndex: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Question = mongoose.model("Question", questionSchema);
+
+export default Question;
