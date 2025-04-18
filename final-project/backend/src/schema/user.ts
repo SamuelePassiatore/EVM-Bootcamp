481e99b4b1eaddce5440b64befc6381fffce3f07
s99100532 <s99100532@gmail.com>
2025-04-18 08:28:52 +0800

    feat: add user schema

---
+++ final-project/backend/src/schema/user.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+interface IUser {
+  username: string;
+  walletAddress: string;
+  createdAt: Date;
+}
+
+const userSchema = new Schema<IUser>({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  walletAddress: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
