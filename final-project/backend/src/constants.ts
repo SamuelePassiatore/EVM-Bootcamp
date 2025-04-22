ab63fd6e27873cd45986482bdcd0eecdb1948934
SamuelePassiatore <samuw.978@gmail.com>
2025-04-22 18:54:13 +0200

    update constants with sepolia chain

--- final-project/backend/src/constants.ts
+++ final-project/backend/src/constants.ts
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-import { hardhat } from "viem/chains";
+import { sepolia } from "viem/chains";
 
 dotenv.config();
 
@@ -8,7 +8,7 @@ const mongodb_uri = process.env.MONGODB_URI;
 const reownProjectId = process.env.REOWN_PROJECT_ID;
 const nftContractAddress = process.env.NFT_CONTRACT_ADDRESS || "";
 const ownerPrivateKey = (process.env.OWNER_PRIVATE_KEY || "") as `0x${string}`;
-const chain = hardhat;
+const chain = sepolia;
 
 if (!ownerPrivateKey || !nftContractAddress) {
   throw new Error("some env not found");
